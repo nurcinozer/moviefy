ba12e3d282d14a6232184838a72f4293afc6bb8d
Nurcin <nurcin@192.168.1.3>
2021-12-12 00:20:12 +0300

    Fix: Folder structure #1 I removed unimportant files in the client folder.

--- client/src/App.js
+++ client/src/App.js
@@ -41,17 +41,8 @@ function App() {
           <Router>
             <ScrollToTop />
             <Switch>
-              <Route path="/top-artists">
-                <h1>Top Artists</h1>
-              </Route>
               <Route path="/top-tracks">
                 <h1>Top Tracks</h1>
-              </Route>
-              <Route path="/playlists/:id">
-                <h1>Playlist</h1>
-              </Route>
-              <Route path="/playlists">
-                <h1>Playlists</h1>
               </Route>
               <Route path="/">
                 <>
